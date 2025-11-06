1c246cde70da8650303cb7e2574feed13779129f
oscar <oscarmancilla227@gmail.com>
2025-11-06 14:08:09 -0600

    awdawdaa2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import Inicio from "./components/inicio/Inicio";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Outlet } from "react-router";
 
